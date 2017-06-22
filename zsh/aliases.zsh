b357127ede888c53ab48209b4714883ac560e441
mathieudutour <mathieu@dutour.me>
2017-06-22 22:38:22 +0200

    add vi alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,7 @@
 alias reload!='. ~/.zshrc'
 
+alias vi="vim"
+
 # Show/hide hidden files in Finder
 alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
 alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
