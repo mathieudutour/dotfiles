caac55eaac438d71b257faf87abaf665aa20341f
mathieudutour <mathieu@dutour.me>
2018-03-20 18:43:13 +0100

    add npm completion

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -212,8 +212,6 @@ sudo mdutil -E / > /dev/null
 
 # Prevent Time Machine from prompting to use new hard drives as backup volume
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
-# Disable local Time Machine backups
-hash tmutil &> /dev/null && sudo tmutil disablelocal
 
 # Use plain text mode for new TextEdit documents
 defaults write com.apple.TextEdit RichText -int 0
