ac7c00415ed2c1b61c7a9be2e2e6bd4b8fe4ba77
mathieudutour <mathieu@dutour.me>
2019-03-18 11:07:37 -0400

    enable dark mode

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -17,6 +17,16 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Set dark mode
+default write NSGlobalDomain AppleInterfaceStyle Dark
+
+# Use non-retard units
+default write NSGlobalDomain AppleMetricUnits 1
+default write NSGlobalDomain AppleTemperatureUnit Celsius
+
+# Always open in a tab
+default write NSGlobalDomain AppleWindowTabbingMode always
+
 # Disable press-and-hold for keys in favor of key repeat.
 # defaults write -g ApplePressAndHoldEnabled -bool false
 
@@ -39,9 +49,6 @@ defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 # Run the screensaver if we're in the bottom-left hot corner.
 # defaults write com.apple.dock wvous-bl-corner -int 5
 # defaults write com.apple.dock wvous-bl-modifier -int 0
-
-# Hide Safari's bookmark bar.
-defaults write com.apple.Safari ShowFavoritesBar -bool false
 
 # Set up Safari for development.
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
