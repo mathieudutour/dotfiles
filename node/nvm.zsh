5d1bd3dc06d6b333f3285615fd53634878ff76b3
mathieudutour <mathieu@dutour.me>
2019-03-18 11:07:47 -0400

    fix nvm path

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,2 +1,3 @@
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
