f245d3750978b1ad71eb6bcc4eb1ea5cdb11f4e3
mathieudutour <mathieu@dutour.me>
2019-05-10 17:54:21 -0400

    install xcode if necesserary

--- macos/install.sh
+++ macos/install.sh
@@ -3,6 +3,15 @@ if test ! "$(uname)" = "Darwin"
   exit 0
 fi
 
+# Install xcode if necesserary
+if type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
+  test -d "${xpath}" && test -x "${xpath}" ; then
+  echo "› Skipping Xcode installation"
+else
+  echo "› xcode-select --install"
+  xcode-select --install
+fi
+
 # The Brewfile handles Homebrew-based app and library installs, but there may
 # still be updates and installables in the Mac App Store. There's a nifty
 # command line interface to it that we can use to just install everything, so
