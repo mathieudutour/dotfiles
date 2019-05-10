28494ebd7b03bd1d560477a7bf129be1dd148f72
mathieudutour <mathieu@dutour.me>
2019-05-10 18:27:27 -0400

    move macos aliases to macos folder

---
+++ macos/aliases.zsh
@@ -0,0 +1,3 @@
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,3 @@
 alias reload!='. ~/.zshrc'
 
 alias vi="vim"
-
-# Show/hide hidden files in Finder
-alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
-alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
